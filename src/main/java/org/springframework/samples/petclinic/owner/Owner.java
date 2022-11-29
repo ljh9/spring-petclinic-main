e4dbb9230695c34ef1f288902f6ca2da40d1008c
ljh9 <dkssud2208@naver.com>
2022-11-29 19:05:43 +0900

    Add column

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -64,6 +64,20 @@ public class Owner extends Person {
 	@OrderBy("name")
 	private List<Pet> pets = new ArrayList<>();
 
+	private Integer age;
+
+	public void setPets(List<Pet> pets) {
+		this.pets = pets;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
 	public String getAddress() {
 		return this.address;
 	}
